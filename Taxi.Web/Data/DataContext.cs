2b7a8059b11851aa538e6973fb7fc5c7b9a22823
alansampaio <alanmangueira@gmail.com>
2020-12-01 20:28:27 -0300

    Adicionado as models  TRip e TripDetails

---
+++ Taxi.Web/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Taxi.Web.Data.Entities;
+
+namespace Taxi.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<TaxiEntity> Taxis { get; set; }
+        public DbSet<TripEntity> Trips { get; set; }
+        public DbSet<TripDetailEntity> TripDetails { get; set; }
+
+    }
+}
---
+++ Taxi.Web/Data/Entities/TaxiEntity.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Taxi.Web.Data.Entities
+{
+    public class TaxiEntity
+    {
+        public int Id { get; set; }
+
+        [RegularExpression(@"^([A-Za-z]{3}\d{3})$", ErrorMessage = "O campo {0} deve ter três caracteres e três números.")]
+        [StringLength(6, MinimumLength = 6, ErrorMessage = "The {0} field must have {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string Plaque { get; set; }
+
+        public ICollection<TripEntity> Trips { get; set; }
+
+    }
+}
